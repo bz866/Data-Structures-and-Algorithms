3001f273b462b5421f4630945c834bc6998621f5
Binqian Zeng <bz866@nyu.edu>
2019-08-19 10:56:17 -0400

    coding ongoing

--- sparseMatrixLlist.py
+++ sparseMatrixLlist.py
@@ -3,3 +3,18 @@ from array import Array
 
 class SparseMatrix:
 	# Creates a sparse matrix of size numRows x numCols initialized to 0
+	def __init__(self, numRows, numCols):
+		self.numCols = numCols
+		self._listOfRows = Array(numRows)
+	
+	# Returns the number of rows in the matrix 
+	def numRows(self):
+		reutrn len(self._listOfRows)
+		
+	# Returns the number of columns in the matrix
+	def numCols(self):
+		return self._numCols
+	
+	# Returns the value of element (i, j): x[i, j]
+	def __getitem__(self, ndxTuple):
+		# TODO
