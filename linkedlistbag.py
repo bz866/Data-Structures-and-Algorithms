4c911a1c0b51e74fe9d7e5ff01d87d003d0d3602
Binqian Zeng <bz866@nyu.edu>
2019-08-19 09:40:21 -0400

    create linked list bag, #TODO: debug
    a.k.a llistbag

--- linkedlistbag.py
+++ linkedlistbag.py
@@ -1,4 +1,54 @@
 # Implements the Bag ADT using a single linked list 
 class Bag:
 	# Construct and empty bag
-	def __init
+	def __init__(self):
+		self._head = None 
+		self._size = 0
+		
+	# Returns the number of items in the bag
+	def __len__(self):
+		return self._size
+	
+	# Determines if an item is contained in the bag
+	def __contains__(self, target):
+		curNode = self._head
+		while (curNode is not None and curNode.item != target):
+			curNode = curNode.next
+		return curNode is not None 
+	
+	# Adds a new item to the bag, add to the head of the linked lisk 
+	def addToHead(self, item):
+		newNode = _BagListNode(item)
+		newNode.next = self._head
+		self._head = newNode
+		self._size += 1
+		
+	# Removes an instance of the item from the bag
+	def remove(self, item):
+		preNode = None 
+		curNode = self._head
+		while (curNode is not None and curNode.item != item):
+			preNode = curNode 
+			curNode = curNode.next
+			
+		# The item to remove has to be in the bag
+		assert curNode is None, "The item to be removed has to be in the linked list bag."
+		
+		# Unlink the node and return the node
+		self._size -= 1
+		if curNode = self._head:
+			self._head = curNode.next
+		else:
+			preNode.next = curNode.next
+		
+		return curNode.item
+	
+	# Returns an iterator for traversing the list of items
+	def __iter__(self):
+		return _LinkedListBagIterator(self._head)
+	
+# Defines a private storage class for creating list nodes
+class _BagListNode(object):
+	def __init__(self, item):
+		self.item = item
+		self.next = None
