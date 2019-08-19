c9ff235310588dccbb079c4c374e4ebc92a3f7d4
Binqian Zeng <bz866@nyu.edu>
2019-08-19 11:57:07 -0400

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -1,2 +1,13 @@
 # Implementation of vector ADT in STL counterpart
 
+# include <algorithm>
+
+template <typename object>
+class Vector
+{
+public:
+	explicit Vector(int initSize = 0) : theSize{initSize}, 
+		theCapacity{initSize + SPARE_CAPACITY}
+	{objects = new Object[theCapacity];}
+	
+	Vector(const Vector & rhs) :
