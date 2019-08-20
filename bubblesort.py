bbcb87d30064f36427abf19de081852d28b11674
bz866 <bz866@nyu.edu>
2019-08-19 21:36:45 -0400

    bubble sort done

---
+++ bubblesort.py
@@ -0,0 +1,23 @@
+# Bubble Sort
+# - Assume the sequence contains n values
+# - iterate the sequence multiple times 
+# - swap values if back > front (bubble) 
+# - Time: O(n^2)
+# - [(n -1) + 1] * [(n - 1 + 1) / 1 + 1] / 2
+# - 0.5 * n^2 + 0.5 * n
+# - Spac: O(1)
+# - sorting done by swapping
+
+# implementation of the bubble sort algorithm
+def bubbleSort(theValues):
+	n = len(theValues)
+	for i in range(0, n - 1):
+		for j in range(0, n - i - 1):
+			if theValues[j] > theValues[j+1]:
+				# Swap
+				temp = theValues[j]
+				theValues[j] = theValues[j+1]
+				theValues[j+1] = temp
+	return theValues
+
+
