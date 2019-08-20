d8921c2237ed6d202cec7efd6813c6a16453a490
bz866 <bz866@nyu.edu>
2019-08-19 21:56:58 -0400

    selection sort done

---
+++ selectionsort.py
@@ -0,0 +1,23 @@
+# Selection Sort 
+# An improvement on the bubble sort
+# - Assume the sequence contains n values 
+# - iterate the sequence multiple times
+# - Only do ONE swap per walking through (improvement on the bubble sort)
+# - always get the smallest value and put it in the head of the sebsequence
+# Time: O(n^2)
+# Space: O(1) 
+
+# Implementation of the selection sort algorithm
+def selectionSort(theValues):
+	n = len(theValues)
+	for i in range(0, n):
+		theSmallestNdx = i
+		for j in range(i, n):
+			if theValues[j] < theValues[theSmallestNdx]:
+				theSmallestNdx = j
+		# Swap the smallest value to the head of the subsequence
+		temp = theValues[i]
+		theValues[i] = theValues[theSmallestNdx]
+		theValues[theSmallestNdx] = temp
+	
+	return theValues
