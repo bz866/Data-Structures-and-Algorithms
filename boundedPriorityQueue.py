e6bc756da746a785391bee39c83efe080a618ed4
Binqian Zeng <bz866@nyu.edu>
2019-08-21 17:55:53 -0400

    writing

--- boundedPriorityQueue.py
+++ boundedPriorityQueue.py
@@ -1,1 +1,12 @@
+# Implementation of the bounded Priority Queue ADT using an array of 
+# queues in which the queues are implemented using a linked list
+from array import Array
+from queuebyLinkedList import Queue
 
+class BoundedPriorityQueue:
+  # Creates an empty bounded priority queue
+  def __init__(self, numLevels):
+    self.qSize = 0
+    self.qLevels = Array(numLevels)
+    for i in range(numLevels):
+      self.qLevels[i] = Queue()
