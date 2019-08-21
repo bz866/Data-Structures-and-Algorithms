64cdc039e6f4a0d30a8fa23f4842fbfeebf4d555
Binqian Zeng <bz866@nyu.edu>
2019-08-21 16:28:02 -0400

    getitem(), setitem(), add()

--- sparseMatrixLinkedList.py
+++ sparseMatrixLinkedList.py
@@ -19,10 +19,94 @@ class sparseMatrixLinkedList:
 	def __getitem__(self, ndxTuple):
 		rowNdx = ndxTuple[0]
 		colNdx = ndxTuple[1]
-		row = self._listOfRows[rowNdx]
+		curNode = self._listOfRows[rowNdx]
+		while curNode is not None and curNode.col != col:
+			curNode = curNode.next
 		
+		# check if the element in the list, if in, get it, if not, return 0.0
+		if curNode is not None and curNode.col == col: # if the node exists
+			return curNode.value
+		elif curNode is None: # if the node doesn't exist, return 0.0 by the definition of a sparse matrix
+			return 0.0
+		
+	# Sets the value of element (i, j) to the value s: x[i,j] = s
+	def __setitem__(self, ndxTuple, value):
+		rowNdx = ndxTuple[0]
+		colNdx = ndxTuple[1]
+		preNode = None
+		curNode = self._listOfRows[row]
+		while curNode is not None and curNode.col != col:
+			preNode = curNode 
+			curNode = curNode.next
+			
+		# check if the element in the list, if in, modify it, if not, add a newNode for the new value
+		if curNode is not None and curNode.col == col: # if the node exist
+			if value == 0.0: # remove the node
+				if curNode == self._listOfRows[row]: # if the node to remove is the head of a row
+					self._listOfRow[row] = curNode.next
+				else: # the node to remove is not the head of a row
+					preNode.next = curNode.next
+			else: # modify the node
+				curNode.value = value
+				
+		elif value !== 0.0: # if the node not exist and the value to assign is not 0.0
+			newNode = _MatrixElementNode(col, value)
+			newNode.next = curNode
+			if curNode == self._listOfRows[row]: # 
+				self._listOfRows[row] = newNode
+			else:
+				preNode.next = newNode
+				
+		else: # the value to assign is 0.0, by the definition of the sparse matrix, we only store nodes with non-zero values
+			pass 
+		
+	# Scales the matrix by the given scalar
+	def scaleBy(self, scalar):
+		for row in range(self.numRows):
+			curNode = self._listOfRows[row]
+			while curNode is not None:
+				curNode.value *= scalar
+				curNode = curNode.next
+				
+	# Creates and returns a new matrix that is the transpose of this matrix
+	def transpose(self):
+		#TODO
+		
+	# Matix addition: newMatrix =  self + rhsMatrix
+	def __add__(self, rhsMatrix):
+		# make sure the two matrix have consistent size
+		assert self.numRows() == rhsMatrix.numRows() and \
+				self.numCols() == rhsMatrix.numCols(), \
+				"Matrix sizes not compatable for adding."
+		
+		# Create a new sparse matrix of the same size
+		newMatrix = SparseMatrix(self.numRows(), self.numCols())
+		
+		# Add the element of this matrix to the new matrix 
+		for row in range(self.numRows()):
+			curNode = self._listOfRows[row]
+			while curNode is not None:
+				newMatrix[row, curNode.col] = curNode.value
+				curNode = curNode.next
+		
+		# Add the elements of the rhsMatrix to the new matrix
+		for row in range(rhsMatrix.numRows()):
+			curNode = rhsMatrix._listOfRows[row]
+			while curNode is not None:
+				value = newMatrix._listOfRows[row, curNode.col]
+				value += curNode.value
+				newMatrix._listOfRows[row, curNode.col] = value
+				curNode = curNode.next
 
-
+		return newMatrix
+	
+	# Matix subtraction: newMatrix =  self - rhsMatrix
+	def __sub__(self, rhsMatrix):
+		# TODO
+	
+	# Matrix multiplication: newMatrix = self * rhsMatrix.T
+	def __mul__(self, rhsMatrix):
+		# TODO
 
 # Storage class for creating matrix element nodes
 class _MatrixElementNode:
