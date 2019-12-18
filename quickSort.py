80ba13ac165d0f5d95f23255e841d8561abec485
bz866 <bz866@nyu.edu>
2019-12-17 23:05:55 -0500

    quickSort update

--- quickSort.py
+++ quickSort.py
@@ -24,9 +24,28 @@ def partition(arr, left, right):
 	arr[i+1], arr[right] = arr[right], arr[i+1]
 	return i + 1
 
+def kLargestNumber(arr, k):
+	left = 0
+	right = len(arr) - 1
+
+	while True:
+		pi = partition(arr, left, right)
+		if pi > k-1:
+			right = pi - 1
+		elif pi < k-1:
+			left = pi + 1
+		else:
+			break
+
+	return arr[pi]
+
 if __name__ == "__main__":
 
 	seq = [1,5,8,2,50,3937,9,0,83,8]
 	print(quickSort(seq, 0, len(seq)-1))
+
+	seq = [1,5,8,2,50,3937,9,0,83,8]
+	assert kLargestNumber(seq, 2) == 83, "Klargest fail"
+	print("DONE")
 
 	
