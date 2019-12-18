5505a74bec680135f20c377c2ac8fab6d770b827
bz866 <bz866@nyu.edu>
2019-12-17 22:56:11 -0500

    quickSort update

--- quickSort.py
+++ quickSort.py
@@ -1,55 +1,31 @@
 # Implementation of the quick sort algorithm
 def quickSort(theList):
-	n = len(theList)
-	recQuickSort(theList, 0, n-1)
+	left = 0
+	right = len(theList) - 1
+
+	if left < right:
+		pi = partition(theList, left, right)
+
+		quickSort(theList, left, pi-1)
+		quickSort(theList, pi+1, right)
+
+	return theList
 
 
-# The recursion implementation using virtual segments
-def recQuickSort(theList, first, last):
-	# Check the base case
-	if first >= last:
-		return 
+def partition(arr, left, right):
+	i = left - 1
+	pivot = arr[right]
 
-	else:
-		# Save the pivot value 
-		pivot = theList[first]
+	for j in range(left, right):
+		if arr[j] > right: # > descending, < ascending
+			i += 1
+			arr[i], arr[j] = arr[j], arr[i]
 
-		# partition the sequence and obtain the pivot position
-		pos = partitionList(theList, first, last)
+	arr[i+1], a[right] = a[right], arr[i+1]
+	return i + 1
 
-		# Repeat the process on the two subsequence
-		recQuickSort(theList, first, pos - 1)
-		recQuickSort(theList, pos + 1, last)
+if __name__ == "__main__":
 
-# partitions the subsequence using the first key as the pivot
-def partitionList(theList, first, last):
-	# Save a copy of the pivot table
-	pivot = theList[first]
-
-	# Find the pivot position and move the elements around the pivot
-	left = first + 1
-	right = last
-	while left <= right:
-		# Find the first key larger than the pivot
-		while left < right and theList[left] < pivot:
-			left += 1
-
-		# Find the last key in the sequence that is smaller than the pivot
-		while right >= left and theList[right] >= pivot:
-			right -= 1
-
-		# Swap the two keys if we have not completed this partition
-		if left < right:
-			tmp = theList[left]
-			theList[left] = theList[right]
-			theList[right] = tmp
-
-	# Put the pivot in the proper position
-	if right != first:
-		theList[first] = theList[right]
-		theList[right] = pivot
-
-	# Returns the index position of the pivot value 
-	return right
+	print(quickSort([1,5,8,2,50,3937,9,0,83,8]))
 
 	
