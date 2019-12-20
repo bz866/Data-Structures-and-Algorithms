d75f2747fdb404d4d497bf851017b77590cd38a3
Binqian Zeng <bz866@nyu.edu>
2019-12-20 10:18:16 -0500

    Update sqrtNewton.py

--- sqrtNewton.py
+++ sqrtNewton.py
@@ -1,31 +1,45 @@
 # a sqrt function without using built in functions
 
-def sqrtNewton(target, x0, numIter):
+def newtonMethod(num, numIters = 500):
+	x = num # initialization
+	for i in range(0, numIters):
+		x = optimizeFunc(num, x) # update by newton method
+		
+	return x
 
-	xNew = x0
-	diff = target - xNew**2
+def optimizeFunc(num, x):
+	return 0.5 * (x + num / x) # optimize function
 
-	for i in range(0, numIter):
-		xNew = iterToUpdate(target, xNew)
-		diff = target - xNew**2
-		if diff == 0:
-			break
-		print("{:.8f}->{:.8f}".format(xNew, target))
-
-	return xNew
-
-
-def iterToUpdate(target, x):
-
-	return x - (target - x**2) / (-2 * x)
+print("{:.8f}".format(newtonMethod(9)))
+print("{:.8f}".format(newtonMethod(2)))
 
 
+# def sqrtNewton(target, x0, numIter):
 
-if __name__ == "__main__":
+# 	xNew = x0
+# 	diff = target - xNew**2
 
-	target = 2.0
-	x0 = 2.0
-	res = sqrtNewton(target, x0, 500)
+# 	for i in range(0, numIter):
+# 		xNew = iterToUpdate(target, xNew)
+# 		diff = target - xNew**2
+# 		if diff == 0:
+# 			break
+# 		print("{:.8f}->{:.8f}".format(xNew, target))
 
-	print("Result: {:.10f}".format(res))
-	print("Sqaure: {:.10f}".format(res**2))
+# 	return xNew
+
+
+# def iterToUpdate(target, x):
+
+# 	return x - (target - x**2) / (-2 * x)
+
+
+
+# if __name__ == "__main__":
+
+# 	target = 2.0
+# 	x0 = 2.0
+# 	res = sqrtNewton(target, x0, 500)
+
+# 	print("Result: {:.10f}".format(res))
+# 	print("Sqaure: {:.10f}".format(res**2))
