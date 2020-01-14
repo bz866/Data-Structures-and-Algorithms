fc9b3e24bcc50646839479a947522370b34a64a3
bz866 <bz866@nyu.edu>
2020-01-13 21:26:51 -0500

    multiple ways

--- binarySearch.py
+++ binarySearch.py
@@ -15,6 +15,22 @@ def binarySearch(arr, left, right, val):
 
 	return False
 
+def binarySearch2(arr, left, right, val):
+	while left <= right:
+		mid = left + (right - left) // 2
+		if arr[mid] == val:
+			return True
+		elif arr[mid] < val:
+			left = mid + 1
+		else:
+			right = mid - 1
+
+	return False
+
+def binarySearchRecursive(arr, left, right, val):
+	if right >= left:
+		mid = 
+
 
 def binaryInsert(arr, left, right, val):
 	# right as len(arr) - 1
@@ -37,4 +53,10 @@ def binaryInsert(arr, left, right, val):
 
 if __name__ == "__main__":
 
+	arr = [2, 3, 4, 10, 40]
+	assert binarySearch(arr, 10) == True, 'Search 1 has bug.'
+	assert binarySearch2(arr, 10) == True, 'Search 2 has bug'
+	assert 
+
+	print('DONE') 
 	
